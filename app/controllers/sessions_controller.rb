0b27d3dfd9aa57e1ca2636d5ca56b1c1ba935ce3
awakems <explo1dms@gmail.com>
2014-03-20 18:31:22 +0200

    update form_tag

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by_email(params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+  	user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password])
       sign_in user
       redirect_to user
     else
@@ -16,5 +16,5 @@ class SessionsController < ApplicationController
   def destroy
     sign_out
     redirect_to root_url
-  end
+  end 
 end
